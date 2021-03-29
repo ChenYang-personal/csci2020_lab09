e1726fb2159b443bafb3283dfa463931472de8a5
Chen Yang <xsyyyccc@gmail.com>
2021-03-29 16:22:33 -0400

    lab09_commit

---
+++ src/sample/Main.java
@@ -0,0 +1,80 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+public class Main extends Application {
+    double google_stock[];
+    double apple_stock[];
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("lab09: Stock Performance");
+        apple_stock=downloadStockPrices("AAPL");
+        google_stock=downloadStockPrices("GOOG");
+        drawLinePlot();
+        primaryStage.setScene(new Scene(new Pane(canvas), 1000, 1000));
+        primaryStage.show();
+    }
+
+    public double[] downloadStockPrices(String company) throws IOException {
+        String website = "https://query1.finance.yahoo.com/v7/finance/download/" +
+                company+"?period1=1262322000&period2=1451538000&interval=1mo&" +
+                "events=history&includeAdjustedClose=true";
+        URL download_data = new URL(website);
+        URLConnection data = download_data.openConnection();
+        Scanner input = new Scanner(data.getInputStream());
+
+
+        double stock_data[]=new double[80];
+        boolean hasnext = true;
+        int count=0;
+        while (input.hasNext()){
+            if(hasnext == true){
+                input.nextLine();
+                hasnext=false;
+            }
+            String list[]=input.nextLine().split(",");
+            stock_data[count] =Double.parseDouble(list[5]);
+            count++;
+        }
+        return stock_data;
+    }
+
+    public void plotLine(Color color,double stock[],GraphicsContext gc){
+        for(int i=0;i<71;i++){
+            double a = 50+i*12;
+            double b = 950 - stock[i];
+            double c = 50+(i+1)*12;
+            double d = 950 - stock[i+1];
+            gc.setStroke(color);
+            gc.strokeLine(a,b,c,d);
+            gc.stroke();
+
+        }
+    }
+
+    Canvas canvas = new Canvas(1000,1000);
+    public void drawLinePlot(){
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setStroke(Color.BLACK);
+        gc.strokeLine(50,950,950,950);
+        gc.strokeLine(50,50,50,950);
+        gc.stroke();
+        plotLine(Color.BLUE,apple_stock,gc);
+        plotLine(Color.RED,google_stock,gc);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
